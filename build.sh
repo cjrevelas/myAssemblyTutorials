2fb7acd89e7b002e255a3dce50978664330a4bdc
cjrevelas <cjrevelas@gmail.com>
2024-11-17 14:58:05 +0200

    docs: function demonstration

--- build.sh
+++ build.sh
@@ -2,3 +2,6 @@
 src_name=$1
 nasm -f elf64 -o ${src_name}.o ${src_name}.s
 gcc -no-pie -m64 -o ${src_name}.x ${src_name}.o
+
+#nasm -f elf -o ${src_name}.o ${src_name}.s
+#ld -m elf_i386 -s -o ${src_name}.x ${src_name}.o
