5d4ea272d3a4c277acf24846c709ad5af15cafac
constantinosrevelas1 <constantinos.revelas@teracloud.com>
2024-11-28 23:04:42 +0200

    add comments for guidance

--- hello.s
+++ hello.s
@@ -1,17 +1,17 @@
-section .data
-	msg db "Hello world!", 0xa
-	len equ $ -msg
+section .data	; data segment
+	msg db "Hello world!", 0xa ; the string to be printed
+	len equ $ -msg             ; length of the string
 
-section .text
-	global main
+section .text	; code segment
+	global main ; declare entry point for the linker
 
-main:
-	mov eax, 4
-	mov ebx, 1
-	mov ecx, msg
-	mov edx, len
-	int 0x80
+main:				; entry point
+	mov eax, 4		; system-call number corresponding to sys_write
+	mov ebx, 1		; file descriptor corresponding to stdout
+	mov ecx, msg	; message to write to stdout
+	mov edx, len	; length of the message
+	int 0x80		; perform an interrupt to invoke kernel functionality
 
-	mov eax, 1
-	mov ebx, 0
-	int 0x80
+	mov eax, 1		; system-call number corresponding to sys_exit
+	mov ebx, 0		; error code
+	int 0x80		; perform an interrupt to terminate program execution
