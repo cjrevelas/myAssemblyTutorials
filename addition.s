6ea5f3420324b63d72c2ec95243e7d1b4425942c
cjrevelas <constantinos.revelas@teracloud.com>
2024-11-28 23:25:50 +0200

    demo adding two numbers

---
+++ addition.s
@@ -0,0 +1,39 @@
+SYS_EXIT equ 1
+SYS_WRITE equ 4
+
+section .data
+    msg db "The sum is:", 0xA, 0xD
+    len equ $-msg
+    
+    segment .bss
+    sum resb    1
+
+section .text
+    global _start
+
+_start:
+    mov eax,'3'
+    sub eax,'0'
+
+    mov ebx,'4'
+    sub ebx,'0'
+
+    add eax,ebx
+    add eax,'0'
+
+    mov [sum],eax ; storing the sum in memory location "sum"
+
+    mov eax,4
+    mov ecx,msg
+    mov edx,len
+    mov ebx,1
+    int 0x80
+
+    mov ecx,sum
+    mov edx,1
+    mov ebx,1
+    mov eax,4
+    int 0x80
+
+    mov eax,1
+    int 0x80    
