e01c94e325e6cab7bd039fd8e074565079f91dec
cjrevelas <constantinos.revelas@teracloud.com>
2024-11-30 18:28:24 +0200

    add new line in the end of the message and use declared constants in eax

--- addition.s
+++ addition.s
@@ -1,39 +1,49 @@
-SYS_EXIT equ 1
-SYS_WRITE equ 4
-
 section .data
-    msg db "The sum is:", 0xA, 0xD
+    SYS_EXIT equ 1
+    SYS_WRITE equ 4
+
+    msg db "The sum is:", 0XA, 0xD
     len equ $-msg
+
+    newLine db 0xA
+    len2 equ $-newLine
     
     segment .bss
-    sum resb    1
+    sum resb 1
 
 section .text
-    global _start
+    global main
 
-_start:
-    mov eax,'3'
-    sub eax,'0'
+main:
+    mov eax, '3'
+    sub eax, '0'
 
-    mov ebx,'4'
-    sub ebx,'0'
+    mov ebx, '4'
+    sub ebx, '0'
 
-    add eax,ebx
-    add eax,'0'
+    add eax, ebx
+    add eax, '0'
 
-    mov [sum],eax ; storing the sum in memory location "sum"
+    mov [sum], eax ; storing the sum in memory location "sum"
 
-    mov eax,4
-    mov ecx,msg
-    mov edx,len
-    mov ebx,1
+    mov eax, SYS_WRITE
+    mov ebx, 1
+    mov ecx, msg
+    mov edx, len
     int 0x80
 
-    mov ecx,sum
-    mov edx,1
-    mov ebx,1
-    mov eax,4
+    mov eax, SYS_WRITE
+    mov ebx, 1
+    mov ecx, sum
+    mov edx, 1
     int 0x80
 
-    mov eax,1
+    mov eax, SYS_WRITE
+    mov ebx, 1
+    mov ecx, newLine
+    mov edx, len2
+    int 0x80
+
+    mov eax, SYS_EXIT
+    mov ebx, 0
     int 0x80    
