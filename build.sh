3c8e64d7cec78e360fa949ecc967c4834d054fca
cjrevelas <constantinos.revelas@teracloud.com>
2024-12-24 13:20:14 +0200

    docs: modify building scripts

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 src_name=$1
-nasm -f elf64 -o ${src_name}.o ${src_name}.s
-gcc -no-pie -m64 -o ${src_name}.x ${src_name}.o
 
-#nasm -f elf -o ${src_name}.o ${src_name}.s
-#ld -m elf_i386 -s -o ${src_name}.x ${src_name}.o
+nasm -g -f elf64 -o ${src_name}.o ${src_name}.s  # nasm assembler of 64-bit program with debug information
+#nasm -g -f elf -o ${src_name}.o ${src_name}.s   # nasm assembler of 32-bit program with debug information
+#yasm -g dwarf2 -f elf64 ${src_name}.s           # yasm assembler of 64-bit program with debug information
+
+gcc -g -no-pie -m64 -o ${src_name}.x ${src_name}.o    # gcc 64-bit linker to create debug executable 
+#ld -g -o ${src_name}.x ${src_name}.o                 # ld  64-bit linker to create debug executable
+#ld -g -m elf_i386 -s -o ${src_name}.x ${src_name}.o  # ld  32-bit linker to create debug executable
--- clean.sh
+++ clean.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rm *.o *.x
+rm *.o *.x *.lst
