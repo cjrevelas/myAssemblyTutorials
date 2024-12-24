4ac4e9a39db0eed7f9b784a0156ee00adb62a59f
cjrevelas <constantinos.revelas@teracloud.com>
2024-12-24 12:48:26 +0200

    (hello): align comments

--- hello.s
+++ hello.s
@@ -1,17 +1,17 @@
-section .data	; data segment
-	msg db "Hello world!", 0xa ; the string to be printed
-	len equ $ -msg             ; length of the string
+section .data                  ; data segment
+    msg db "Hello world!", 0xa ; the string to be printed
+    len equ $ -msg             ; length of the string
 
-section .text	; code segment
-	global main ; declare entry point for the linker
+section .text                  ; code segment
+    global main                ; declare entry point for the linker
 
-main:				; entry point
-	mov eax, 4		; system-call number corresponding to sys_write
-	mov ebx, 1		; file descriptor corresponding to stdout
-	mov ecx, msg	; message to write to stdout
-	mov edx, len	; length of the message
-	int 0x80		; perform an interrupt to invoke kernel functionality
+main:                          ; entry point
+    mov eax, 4                 ; system-call number corresponding to sys_write
+    mov ebx, 1                 ; file descriptor corresponding to stdout
+    mov ecx, msg               ; message to write to stdout
+    mov edx, len               ; length of the message
+    int 0x80                   ; perform an interrupt to invoke kernel functionality
 
-	mov eax, 1		; system-call number corresponding to sys_exit
-	mov ebx, 0		; error code
-	int 0x80		; perform an interrupt to terminate program execution
+    mov eax, 1                 ; system-call number corresponding to sys_exit
+    mov ebx, 0                 ; error code
+    int 0x80                   ; perform an interrupt to terminate program execution
