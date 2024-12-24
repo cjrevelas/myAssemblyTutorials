d9952f4fd843e1b46e9d0e753d414a01d0117d73
cjrevelas <constantinos.revelas@teracloud.com>
2024-12-24 17:42:42 +0200

    (hello_with_functions): add stack frame

--- hello_with_functions.s
+++ hello_with_functions.s
@@ -6,17 +6,22 @@ section .text                  ; code segment
     global main                ; declare "main" entry point for the linker
 
 print:
-    mov eax, 4                 ; system-call number corresponding to sys_write
-    mov ebx, 1                 ; file descriptor corresponding to stdout
-    mov ecx, msg               ; message to write to stdout/console
-    mov edx, len               ; length of the message
-    int 0x80                   ; perform an interrupt to invoke kernel functionality
+    push rbp                    ; create stack frame
+    mov  rbp, rsp               ; create stack frame
+    mov  eax, 4                 ; system-call number corresponding to sys_write
+    mov  ebx, 1                 ; file descriptor corresponding to stdout
+    mov  ecx, msg               ; message to write to stdout/console
+    mov  edx, len               ; length of the message
+    int  0x80                   ; perform an interrupt to invoke kernel functionality
+    pop  rbp                    ; restore stack frame
     ret
 
 exit:
-    mov eax, 1                 ; system-call number corresponding to sys_exit
-    mov ebx, 0                 ; error code
-    int 0x80                   ; perform an interrupt to terminate program execution
+    push rbp                   ; create stack frame
+    mov  rbp, rsp              ; create stack frame
+    mov  eax, 1                ; system-call number corresponding to sys_exit
+    mov  ebx, 0                ; error code
+    int  0x80                  ; perform an interrupt to terminate program execution
 
 main:                          ; entry point
     call print                 ; print hello world to the console
