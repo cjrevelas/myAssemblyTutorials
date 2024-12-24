5d925bae5023b8494a2872361a1a08563da71455
cjrevelas <constantinos.revelas@teracloud.com>
2024-12-24 12:55:30 +0200

    demo (hello): print hello via function call

---
+++ hello_with_functions.s
@@ -0,0 +1,23 @@
+section .data                  ; data segment
+    msg db "Hello world!", 0xa ; the string to be printed
+    len equ $ -msg             ; length of the string
+
+section .text                  ; code segment
+    global main                ; declare "main" entry point for the linker
+
+print:
+    mov eax, 4                 ; system-call number corresponding to sys_write
+    mov ebx, 1                 ; file descriptor corresponding to stdout
+    mov ecx, msg               ; message to write to stdout/console
+    mov edx, len               ; length of the message
+    int 0x80                   ; perform an interrupt to invoke kernel functionality
+    ret
+
+exit:
+    mov eax, 1                 ; system-call number corresponding to sys_exit
+    mov ebx, 0                 ; error code
+    int 0x80                   ; perform an interrupt to terminate program execution
+
+main:                          ; entry point
+    call print                 ; print hello world to the console
+    call exit                  ; terminate program execution
