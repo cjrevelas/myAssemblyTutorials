0803d968284c7608bc3b14a04ed55eadacc5881f
cjrevelas <cjrevelas@gmail.com>
2025-09-12 20:54:45 +0300

    add macro definition variables

--- hello.s
+++ hello.s
@@ -1,3 +1,7 @@
+%define SYSCALL_WRITE 4
+%define SYSCALL_EXIT 1
+%define STD_OUT 1
+
 section .data                  ; data segment
     msg db "Hello world!", 0xa ; the string to be printed
     len equ $ -msg             ; length of the string
@@ -6,12 +10,12 @@ section .text                  ; code segment
     global main                ; declare entry point for the linker
 
 main:                          ; entry point
-    mov eax, 4                 ; system-call number corresponding to sys_write
-    mov ebx, 1                 ; file descriptor corresponding to stdout
+    mov eax, SYSCALL_WRITE     ; system-call number corresponding to sys_write
+    mov ebx, STD_OUT           ; file descriptor corresponding to stdout
     mov ecx, msg               ; message to write to stdout
     mov edx, len               ; length of the message
     int 0x80                   ; perform an interrupt to invoke kernel functionality
 
-    mov eax, 1                 ; system-call number corresponding to sys_exit
+    mov eax, SYSCALL_EXIT      ; system-call number corresponding to sys_exit
     mov ebx, 0                 ; error code
     int 0x80                   ; perform an interrupt to terminate program execution
