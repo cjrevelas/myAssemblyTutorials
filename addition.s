6eab60634fc779184e22a95b0dd8078373071c25
cjrevelas <cjrevelas@gmail.com>
2025-09-27 12:52:51 +0300

    create a printing function

--- addition.s
+++ addition.s
@@ -9,42 +9,79 @@ section .data
     newLine db 0xa
     len2 equ $-newLine
 
+    arg1 equ 0
+    arg2 equ 8
+
+
 section .bss
     sum resb 1
+
 
 section .text
     global main
 
+print:
+    push rbp
+    mov rbp, rsp
+
+    sub rsp, 16
+
+    mov [rsp+arg1], rdi
+    mov [rsp+arg2], rsi
+
+    mov rax, SYSCALL_WRITE
+    mov rdi, STD_OUT
+    mov rsi, [rsp+arg1]
+    mov rdx, [rsp+arg2]
+    syscall
+
+    mov rsp, rbp
+    pop rbp
+    ret
+
+exit:
+
 main:
-    mov eax, '3'
-    sub eax, '0'
+    mov rax, '3'
+    sub rax, '0'
 
-    mov ebx, '4'
-    sub ebx, '0'
+    mov rbx, '4'
+    sub rbx, '0'
 
-    add eax, ebx
-    add eax, '0'
+    add rax, rbx
+    add rax, '0'
 
-    mov [sum], eax ; storing the sum in memory location "sum"
+    mov [sum], rax ; storing the sum in memory location "sum"
 
-    mov eax, SYSCALL_WRITE
-    mov edi, STD_OUT
-    mov esi, msg
-    mov edx, len
-    syscall
+   ; mov eax, SYSCALL_WRITE
+   ; mov edi, STD_OUT
+   ; mov esi, msg
+   ; mov edx, len
+   ; syscall
 
-    mov eax, SYSCALL_WRITE
-    mov edi, STD_OUT
-    mov esi, sum
-    mov edx, len
-    syscall
+    mov rdi, msg
+    mov rsi, len
+    call print
 
-    mov eax, SYSCALL_WRITE
-    mov edi, STD_OUT
-    mov esi, newLine
-    mov edx, len2
-    syscall
+   ; mov eax, SYSCALL_WRITE
+   ; mov edi, STD_OUT
+   ; mov esi, sum
+   ; mov edx, len
+   ; syscall
 
-    mov eax, SYSCALL_EXIT
-    mov edi, 0
+   mov rdi, sum
+   mov rsi, len
+   call print
+   ; mov eax, SYSCALL_WRITE
+   ; mov edi, STD_OUT
+   ; mov esi, newLine
+   ; mov edx, len2
+   ; syscall
+
+    mov rdi, newLine
+    mov rsi, len2
+    call print
+
+    mov rax, SYSCALL_EXIT
+    mov rdi, 0
     syscall
