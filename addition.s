be4226b60335e4c451c0214cdb4e69842bd16c8d
cjrevelas <cjrevelas@gmail.com>
2025-09-27 11:54:31 +0300

    set an STD_EXIT symbol

--- addition.s
+++ addition.s
@@ -1,14 +1,15 @@
 section .data
-    SYS_EXIT equ 1
-    SYS_WRITE equ 4
+    SYSCALL_EXIT equ 1
+    SYSCALL_WRITE equ 4
+    STD_OUT equ 1
 
-    msg db "The sum is:", 0XA, 0xD
+    msg db "The sum is:", 0xa, 0xd
     len equ $-msg
-
-    newLine db 0xA
-    len2 equ $-newLine
     
-    segment .bss
+    newLine db 0xa
+    len2 equ $-newLine
+
+section .bss
     sum resb 1
 
 section .text
@@ -26,24 +27,24 @@ main:
 
     mov [sum], eax ; storing the sum in memory location "sum"
 
-    mov eax, SYS_WRITE
-    mov ebx, 1
+    mov eax, SYSCALL_WRITE
+    mov ebx, STD_OUT
     mov ecx, msg
     mov edx, len
     int 0x80
 
-    mov eax, SYS_WRITE
-    mov ebx, 1
+    mov eax, SYSCALL_WRITE
+    mov ebx, STD_OUT
     mov ecx, sum
     mov edx, 1
     int 0x80
 
-    mov eax, SYS_WRITE
-    mov ebx, 1
+    mov eax, SYSCALL_WRITE
+    mov ebx, STD_OUT
     mov ecx, newLine
     mov edx, len2
     int 0x80
 
-    mov eax, SYS_EXIT
+    mov eax, SYSCALL_EXIT
     mov ebx, 0
     int 0x80    
