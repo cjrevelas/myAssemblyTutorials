7af2b0662ce71553b9c5e07fe8dfbea29e5b1701
cjrevelas <cjrevelas@gmail.com>
2025-10-11 17:00:03 +0300

    rename some assembly modules

--- hello_syscall.s
+++ hello_syscall.s
@@ -10,14 +10,12 @@ section .text                  ; code segment
     global main                ; declare entry point for the linker
 
 main:                          ; entry point
-    ; WRITE system call
     mov rax, SYSCALL_WRITE     ; WRITE system call will be performed
     mov rdi, STD_OUT           ; file descriptor corresponding to stdout
     lea rsi, [msg]             ; message to write to stdout
     mov rdx, len               ; length of the message
     syscall                    ; perform the system call
 
-    ; EXIT system call
     mov rax, SYSCALL_EXIT      ; system-call number corresponding to sys_exit
     mov rdi, 0                 ; error code
     syscall                    ; perform the system call
